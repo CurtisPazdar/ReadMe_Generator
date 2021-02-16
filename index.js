2fbb05c60fcf2fbb3f8eb394ba38fc99435b42cc
Curtis P <curtis.pazdar@gmail.com>
2021-02-15 17:08:52 -0700

    TAKING A LOOK

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 fs = require('fs');
 
 var inquirer = require('inquirer');
+const template = require("./template")
 
 
 inquirer
@@ -48,54 +49,14 @@ inquirer
       },
   ])
   .then(answers => {
-    fs.writeFile("README.MD",`<h1>
-    ${answers.projectTitle}
-    </h1>
-    <br>
-    <h3>Table of Contents:
-        ## Table of Contents
-        - [Description](#description)
-        - [Table of Contents](#table-of-contents)
-        - [Installation](#installation)
-        - [Usage](#usage)
-        - [License](#license)
-        - [Contributing](#contributing)
-        - [Tests](#tests)
-        - [Questions](#questions)
-    <br>
-    <h2 id="description">Application Description</h2>
-    <p>
-        ${answers.description}
-    </p>
-    <br>
-    <h3>Installation Instructions:</h3>
-    <p>${answers.installationInstructions}</p>
-    <br>
-    <h3>License Type:</h3>
-    <p>${answers.license}</p>
-    <br>
-    <h3>Test Instructions:</h3>
-    <p>${answers.test}</p>
-    <br>
-    <h3>Contributors</h3>
-    <p>${answers.contributors}</p>
-    <br>
-    <h3>GitHub Accounts</h3>
-    <p>${answers.git}</p>
-    <br>
-    <h3>Email</h3>
-    <p>${answers.email}</p>
-    `, function (err,data) {
+
+    const content = template(answers);
+
+    fs.writeFile("README.MD",content,function (err,data) {
       if (err) {
         return console.log(err);
       }
       console.log(data);
     });
-  })
-  .catch(error => {
-    if(error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else when wrong
-    }
   });
+ 
